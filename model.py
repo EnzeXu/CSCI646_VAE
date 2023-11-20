851812d4483fdc0dbab0a75da80ab5f2adcc7f2c
EnzeXu <xue20@wfu.edu>
2023-11-20 13:17:16 -0500

    update module

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 
 import torch
 import torch.nn as nn
-#import torch.nn.functional as F
+import torch.nn.functional as F
 import torch.nn.init as init
 from torch.autograd import Variable
 
@@ -54,6 +54,22 @@ class VAE(nn.Module):
         self.z_dim = z_dim
         self.nc = nc
 
+        # Encoder
+        self.enc_conv1 = nn.Conv2d(nc, 8, 3, 2, 1)  # B, 8, 14, 14
+        self.enc_conv2 = nn.Conv2d(8, 16, 3, 2, 1)  # B, 16, 7, 7
+        self.enc_conv3 = nn.Conv2d(16, 32, 3, 2)  # B, 32, 3, 3
+        self.enc_fc1 = nn.Linear(32 * 3 * 3, 128)  # B, 128
+        self.enc_mean = nn.Linear(128, z_dim)  # B, z_dim
+        self.enc_std = nn.Linear(128, z_dim)  # B, z_dim
+        # Decoder
+        self.dec_fc1 = nn.Linear(z_dim, 128)  # B, 128
+        self.dec_fc2 = nn.Linear(128, 32 * 3 * 3)  # B, 32*3*3
+        self.dec_convt1 = nn.ConvTranspose2d(32, 16, 3, 2)  # B, 16, 7, 7
+        self.dec_convt2 = nn.ConvTranspose2d(16, 8, 3, 2, 1, 1)  # B, 8, 14, 14
+        self.dec_convt3 = nn.ConvTranspose2d(8, nc, 3, 2, 1, 1)  # B, 1, 28, 28
+        # BN
+        self.bn = nn.BatchNorm2d(16)
+
         self.encoder = nn.Sequential(
             nn.Conv2d(nc, 32, 4, 2, 1),          # B,  32, 32, 32
             nn.ReLU(True),
@@ -68,6 +84,8 @@ class VAE(nn.Module):
             View((-1, 256*1*1)),                 # B, 256
             nn.Linear(256, z_dim*2),             # B, z_dim*2
         )
+
+
 
         self.decoder = nn.Sequential(
             nn.Linear(z_dim, 256),               # B, 256
@@ -93,6 +111,27 @@ class VAE(nn.Module):
             for m in self._modules[block]:
                 kaiming_init(m)
 
+    def _encoder(self, x):
+        x = F.relu(self.enc_conv1(x.view(-1, self.nc, 28, 28)))
+        x = F.relu(self.enc_conv2(x))
+        x = self.bn(x)
+        x = F.relu(self.enc_conv3(x))
+        x = F.relu(self.enc_fc1(x.view(-1, 32 * 3 * 3)))
+        mean = self.enc_mean(x)
+        std = F.softplus(self.enc_std(x))
+        return mean, std
+
+    def _decoder(self, z):
+        x = F.relu(self.dec_fc1(z))
+        x = F.relu(self.dec_fc2(x))
+        x = F.relu(self.dec_convt1(x.view(-1, 32, 3, 3)))
+        x = self.bn(x)
+        x = F.relu(self.dec_convt2(x))
+        x = self.dec_convt3(x)
+        # Using sigmoid function so that the output should be in [0,1]
+        x = torch.sigmoid(x.view(-1, self.nc * 28 * 28))
+        return x
+
     def forward(self, x):
         distributions = self._encode(x)
         mu = distributions[:, :self.z_dim]
@@ -104,11 +143,11 @@ class VAE(nn.Module):
 
         return x_recon, mu, logvar
 
-    def _encode(self, x):
-        return self.encoder(x)
-
-    def _decode(self, z):
-        return self.decoder(z)
+    # def _encode(self, x):
+    #     return self.encoder(x)
+    #
+    # def _decode(self, z):
+    #     return self.decoder(z)
 
 
 def kaiming_init(m):
